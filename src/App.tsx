9be66bc5ea8aa5b6cee04b9bc4f64146bc40cf9a
Isaias Medina <isamed92@gmail.com>
2023-12-20 01:20:36 -0300

    feat: add intl config and favicon svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import { useState } from 'react';
+import { Button, Container } from 'react-bootstrap';
+import { FormattedMessage, IntlProvider } from 'react-intl';
+import { LOCALES, messages } from './i18n';
+
 function App() {
-  return <>Hola mundo</>;
+  const [locale, setLocale] = useState(LOCALES.ENGLISH);
+
+  return (
+    <IntlProvider
+      messages={messages[locale]}
+      locale={locale}
+      defaultLocale={LOCALES.ENGLISH}
+    >
+      <Container>
+        <h1>
+          <FormattedMessage id='title' />
+        </h1>
+        <p>
+          <FormattedMessage id='description' />
+        </p>
+      </Container>
+      <Container>
+        <Button onClick={() => setLocale(LOCALES.ENGLISH)} className='mx-2'>
+          English
+        </Button>
+
+        <Button onClick={() => setLocale(LOCALES.SPANISH)} className='mx-2'>
+          Spanish
+        </Button>
+      </Container>
+    </IntlProvider>
+  );
 }
 
 export default App;
---
+++ src/i18n/messages.ts
@@ -0,0 +1,12 @@
+import { LOCALES } from './locales';
+
+export const messages = {
+  [LOCALES.ENGLISH]: {
+    title: 'Hello, World!',
+    description: 'This is a simple example of React Intl.',
+  },
+  [LOCALES.SPANISH]: {
+    title: '¡Hola, Mundo!',
+    description: 'Este es un ejemplo sencillo de React Intl.',
+  },
+};
