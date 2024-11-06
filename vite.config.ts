16e3d8aad5df8cee1bf0722b29d7b096754e9797
Isaias Medina <isamed92@gmail.com>
2024-11-06 08:29:22 -0300

    update: rollback and  update package json scripts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  root: 'src',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
+  base: 'https://isamed92.github.io/github-pages',
 });
