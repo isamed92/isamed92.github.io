864a0299a88dbb17bd0cee2581f7a4eb9352f340
Isaias Medina <isamed92@gmail.com>
2025-03-04 12:26:29 -0300

    feat: add router

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
+import { Router } from './router/Router';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <div className='main_body'>
-      <App />
+      <Router />
     </div>
   </React.StrictMode>
 );
